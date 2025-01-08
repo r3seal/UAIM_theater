d7b8304a2631c859604a7b9e28c8a55a378dec9e
SealyPrograming <lafar@DRAK01.(none)>
2025-01-08 22:20:19 +0100

    added website files

--- frontend/index.js
+++ frontend/index.js
@@ -2,8 +2,20 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { createRoot } from 'react-dom/client'; // Dla web
 
-AppRegistry.registerComponent(appName, () => App);
+// Sprawdzenie, czy kod działa w przeglądarce (web)
+if (typeof document !== 'undefined') {
+  const container = document.getElementById('root') || document.createElement('div');
+  if (!container.parentElement) {
+    document.body.appendChild(container);
+  }
+  const root = createRoot(container); // React 18+
+  root.render(<App />);
+} else {
+  // Rejestracja aplikacji mobilnej
+  AppRegistry.registerComponent(appName, () => App);
+}
