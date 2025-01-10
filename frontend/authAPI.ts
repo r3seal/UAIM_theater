e88d29d8576251898ed81ebd3b3e921fc1d5393e
Adam Lipian <adamlipian2723@gmail.com>
2025-01-10 19:58:16 +0100

    feat: add logout handling

--- frontend/authAPI.ts
+++ frontend/authAPI.ts
@@ -4,11 +4,28 @@ import urlAPI from './urlAPI';
 
 export const login = async (email: string, password: string) => {
   try {
-    const response = await axios.post(`${urlAPI}:5000/login`, { email, password });
-    await AsyncStorage.setItem('accessToken', response.data.access_token);
-    await AsyncStorage.setItem('refreshToken', response.data.refresh_token);
-    return response.data;
+    const response = await axios.post(`${urlAPI}:5000/auth/login`, { email, password });
+    if (response.status === 200) {
+      await AsyncStorage.setItem('accessToken', response.data.access_token);
+      await AsyncStorage.setItem('refreshToken', response.data.refresh_token);
+      await AsyncStorage.setItem('permission', response.data.permission);
+    }
+    return response.status;
   } catch (error) {
     throw new Error('Login failed');
   }
 };
+
+export const logout = async () => {
+  try {
+    const response = await axios.post(`${urlAPI}:5000/auth/logout`);
+    if (response.status === 200) {
+      await AsyncStorage.clear();
+    }
+    return response.status;
+  } catch (error) {
+    throw new Error('Logout failed');
+  }
+}
+
+
--- frontend/ticketApi.ts
+++ frontend/ticketApi.ts
@@ -1,15 +1,21 @@
 import axios from 'axios';
 import urlAPI from './urlAPI';
 
-export const buyTickets = async (token: string, seatIds: number[], spectacleId: number) => {
+export const buyTickets = async (token: string | null, seatIds: number[], spectacleId: number) => {
   try {
     const response = await axios.post(
-      `${urlAPI}:5000/spectacles/buy`,
-      { seat_ids: seatIds, spectacle_id: spectacleId },
-      { headers: { Authorization: `Bearer ${token}` } }
+        `${urlAPI}:5000/spectacles/buy`,
+        { seat_ids: seatIds, spectacle_id: spectacleId },
+        { headers: { Authorization: `Bearer ${token}` }, withCredentials: true }
     );
+    console.log(response.data);
     return response.data;
   } catch (error) {
-    throw new Error('Ticket purchase failed');
+    if (axios.isAxiosError(error) && error.response) {
+      console.error("Error response:", error.response.data);
+      throw new Error(error.response.data.error || 'Ticket purchase failed');
+    }
+    console.error("Unexpected error:", error);
+    throw new Error('Unexpected error occurred');
   }
 };
